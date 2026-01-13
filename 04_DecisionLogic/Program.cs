4accc69316b0dc5dd4d79152e88df18c935d4d1e
Esat Comlekci <esatcomlekci@gmail.com>
2026-01-13 03:24:46 +0300

    Else IF logic added

--- 04_DecisionLogic/Program.cs
+++ 04_DecisionLogic/Program.cs
@@ -10,28 +10,35 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1))
 {
-    total += 2;
-    Console.WriteLine($"You rolled doubles! +2 bonus to total. New total {total}.");
-    
-}
-
-if((roll1 == roll2) && (roll2 == roll3))
+    if((roll1 == roll2) && (roll2 == roll3))
 {
     total +=6;
     Console.WriteLine($"You rolled triples! +6 BONUS TO TOTAL. New total ${total}");
-
+}
+  else
+  {
+    total += 2;
+    Console.WriteLine($"You rolled doubles! +2 bonus to total. New total {total}.");
+  }
 }
 
-if (total > 14)
+
+if (total >= 16)
 {
-    Console.WriteLine("Thats it!");
+    Console.WriteLine("DAMN SON! You broke the odds.");
 }
-
-if (total < 15)
+else if (total >= 10)
 {
-    Console.WriteLine("End of the road...");
+    Console.WriteLine("Not bad son.");
 }
-
+else if (total == 7)
+{
+    Console.WriteLine("777 Make a wish");
+}
+else
+{
+    Console.WriteLine("Have a glass of water");
+}
 
 
 
